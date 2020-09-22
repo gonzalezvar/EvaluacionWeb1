37fd85ef285f66a204a17ad11a547a31f401bdfc
Juan David <jgonzalezvarilla@gmail.com>
2020-09-21 19:27:33 -0500

    ejercicio 1
    se termina el ejercicio 1

--- index.php
+++ index.php
@@ -1,40 +1,97 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SuperMercado</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+    <header>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <a class="navbar-brand" href="#">Calculadora</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-      <section>
-            <form action="index.php" method="POST">
-                <input class="form-control form-control-lg" type="text" placeholder="num1">
-                <input class="form-control form-control-sm" type="text" placeholder="num2">
-                <button class="submit" name="multplicar">multiplicación</button>
-                <button class="submit" name="sumar">Suma</button>
-                <button class="submit" name="restar">Resta</button>
-            </form>
-        </section>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li> 
+                    </ul>
+                <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+                </div>
+             </nav>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-  </body>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-4">
+                <form class="mt-5" action="index.php" method="POST">
+                    <h4 class="text-center">Calculator</h4>
+                    <div class="row">
+                        <div class="col">
+                        <input type="number" class="form-control" placeholder="Numero1" name="num1">
+                        </div>
+                        <div class="col">
+                        <input type="number" class="form-control" placeholder="Numero2" name="num2">
+                        </div>
+                        <div class="col">
+                        <select class="form-control" id="exampleFormControlSelect1" name="operaciones">
+                            <option value="1">Suma</option>
+                            <option value="2">Resta</option>
+                            <option value="3">Mult</option>
+                        </select>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary  mt-5" name="botonEnvio">submit</button>
+                </form>
+
+                <?php if(isset($_POST["botonEnvio"])):?>
+
+                <h4 class="mt-4">
+                    <?php 
+                       $numero1=$_POST["num1"];
+                       $numero2=$_POST["num2"];
+                       $operacion=$_POST["operaciones"];
+                       switch($operacion)
+                       {
+                        case 1: $resultado=$numero1 + $numero2;
+                        echo ("La Suma de ".$numero1." y ".$numero2." es igual a: ".$resultado);
+                        break;
+                        case 2: $resultado=$numero1 - $numero2;
+                        echo ("La Resta de ".$numero1." y ".$numero2." es igual a: ".$resultado);
+                        break;
+                        case 3: $resultado=$numero1 * $numero2;
+                        echo ("La Multiplicación de ".$numero1." y ".$numero2." es igual a: ".$resultado);
+                        break;
+                       }
+
+                    ?>
+                </h4>
+
+                <?php endif ?>
+
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+
+
+    </footer>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
 </html>
-
-<?php  if(isset($_POST["botonEnvio"])):?>
-
-
-<?php 
-    $numero1=$_POST["num1"];
-    $numero2=$_POST["num2"];
-
-
-    
-?>
-
-
-<?php endif?>
